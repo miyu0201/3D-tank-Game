644bc6caf52693aab97e081c75d64614477f77f5
miyu0201 <y.minghui@yahoo.com>
2021-06-24 17:53:23 +0200

    first commit

---
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float m_DampTime = 0.2f;    //time to move camera to position             
+    public float m_ScreenEdgeBuffer = 4f;   //make sure tank will not be outside screen   
+    public float m_MinSize = 6.5f;     //minimum size of camera             
+    /* [HideInInspector]*/ public Transform[] m_Targets; 
+
+
+    private Camera m_Camera;                        
+    private float m_ZoomSpeed;                      
+    private Vector3 m_MoveVelocity;                 
+    private Vector3 m_DesiredPosition;   //target position to movev to     
+
+
+    private void Awake()
+    {
+        m_Camera = GetComponentInChildren<Camera>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        Move();
+        Zoom();
+    }
+
+
+    private void Move()
+    {
+        FindAveragePosition();
+        /**
+        * function Vector3.SmoothDamp(Vector3 current,Vector3 target
+        *                 ,ref Vector3 currentVelocity,float smoothTime)
+        *  current:current location
+        *  target:target location
+        *  currentVelocity:current speed，value changed by everytimem you call the function
+        *  smoothTime:time wen getto target，the samller value the quicker
+        */
+
+        transform.position = Vector3.SmoothDamp(transform.position, m_DesiredPosition, ref m_MoveVelocity, m_DampTime);
+    }
+
+
+    private void FindAveragePosition()
+    {
+        Vector3 averagePos = new Vector3();
+        int numTargets = 0;
+        //check if tank is inactive(dead), theh no need to follow tank
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+            //makesure tank Y position will not be changed
+            averagePos += m_Targets[i].position;
+            numTargets++;
+        }
+
+        if (numTargets > 0)
+            averagePos /= numTargets;
+
+        averagePos.y = transform.position.y;
+
+        m_DesiredPosition = averagePos;
+    }
+
+
+    private void Zoom()
+    {
+       // based on current location to get size
+        float requiredSize = FindRequiredSize();
+        m_Camera.orthographicSize = Mathf.SmoothDamp(m_Camera.orthographicSize, requiredSize, ref m_ZoomSpeed, m_DampTime);
+    }
+
+
+    private float FindRequiredSize()
+    {
+        //changed target location from world location to local loaction
+        Vector3 desiredLocalPos = transform.InverseTransformPoint(m_DesiredPosition);
+
+        float size = 0f;
+
+        for (int i = 0; i < m_Targets.Length; i++)
+        {
+            if (!m_Targets[i].gameObject.activeSelf)
+                continue;
+            //change tank location to cameraRig local location
+            Vector3 targetLocalPos = transform.InverseTransformPoint(m_Targets[i].position);
+
+            //under cameraRig location, get the distance between tank and cameraRig target location
+            Vector3 desiredPosToTarget = targetLocalPos - desiredLocalPos;
+
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.y));
+
+            size = Mathf.Max (size, Mathf.Abs (desiredPosToTarget.x) / m_Camera.aspect);
+        }
+        //add ScreenEdgeBuffer value, distance between tank and screen edge
+        size += m_ScreenEdgeBuffer;
+
+        size = Mathf.Max(size, m_MinSize);
+
+        return size;
+    }
+
+
+    public void SetStartPositionAndSize()
+    {
+        FindAveragePosition();
+
+        transform.position = m_DesiredPosition;
+
+        m_Camera.orthographicSize = FindRequiredSize();
+    }
+}
---
+++ Assets/Scripts/Tank/TankHealth.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealth : MonoBehaviour
+{
+    public float m_StartingHealth = 100f;   //starting health value       
+    public Slider m_Slider;     //life slider                   
+    public Image m_FillImage;    //fill theh slider                  
+    public Color m_FullHealthColor = Color.green;  
+    public Color m_ZeroHealthColor = Color.red;    //low health red color
+    public GameObject m_ExplosionPrefab;
+    
+    
+    private AudioSource m_ExplosionAudio;   //audio   
+    private ParticleSystem m_ExplosionParticles;   //explosion effect
+    private float m_CurrentHealth;  
+    private bool m_Dead;   //check if dead         
+
+
+    private void Awake()
+    {
+        //prepare example of explosion effect at awake (initial)
+        m_ExplosionParticles = Instantiate(m_ExplosionPrefab).GetComponent<ParticleSystem>();
+        m_ExplosionAudio = m_ExplosionParticles.GetComponent<AudioSource>();
+        //set inactive
+        m_ExplosionParticles.gameObject.SetActive(false);
+    }
+
+
+    private void OnEnable()
+    {
+        m_CurrentHealth = m_StartingHealth;
+        m_Dead = false;
+
+        SetHealthUI();
+    }
+    
+    //call from outside when tank is demaged
+
+    public void TakeDamage(float amount)
+    {
+        // Adjust the tank's current health, update the UI based on the new health and check whether or not the tank is dead.
+        m_CurrentHealth -= amount;
+
+        SetHealthUI();
+
+        //if tank currentHealth <0, is dead
+        if (m_CurrentHealth <= 0f && !m_Dead)
+        {
+            OnDeath();
+        }
+    }
+
+
+    private void SetHealthUI()
+    {
+        // Adjust the value and colour of the slider.
+        m_Slider.value = m_CurrentHealth;
+
+        //change fill color
+        //Color.Lerp Linear interpolation of colors, interpolating between colors 1 and 2 through the third parameter.
+        m_FillImage.color = Color.Lerp(m_ZeroHealthColor, m_FullHealthColor, m_CurrentHealth / m_StartingHealth);
+    }
+
+
+    private void OnDeath()
+    {
+        // Play the effects for the death of the tank and deactivate it.
+        m_Dead = true;
+
+        //set explosion location as tank dead location
+        m_ExplosionParticles.transform.position = transform.position;
+        m_ExplosionParticles.gameObject.SetActive(true);
+
+        //play explosion effect and sound
+        m_ExplosionParticles.Play();
+        m_ExplosionAudio.Play();
+
+        gameObject.SetActive(false);
+
+    }
+}
